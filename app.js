024156a9239246bd10b9a4bd6d541b623a269e49
Delgerjargal0508 <deegii0584@gmail.com.com>
2024-11-12 15:50:42 +0800

    sda

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-let Submit = document.getElementById('SubmitButton');
-let toDoContainer = document.getElementById('taskContainer');
-let inputField = document.getElementById('inputField');
+var tasks = [];
+var submitButton = document.getElementById('btn');
+var inputField = document.getElementById('input');
+var taskList = document.getElementById('task-list')
 
-Submit.addEventListener('click', function(){
-    var paragraph = document.createElement('p')
-    paragraph.innerHTML=inputField.value
-    toDoContainer.appendChild(paragraph);
-    inputField.value = ""
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(paragraph);
-    })
-})
+submitButton.addEventListener('click', addTask);
+
+
+function addTask(){
+    var newTask = inputField.value.trim();
+    if(!newTask) return;
+    tasks.push(newTask);
+    inputField.value = '';
+
+    var li = document.createElement('li');
+    li.textContent = newTask;
+    li.setAttribute('data-index', index);
+    taskList.appendChild(li);
+
+    var editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    li.appendChild(editButton);
+    
+    var deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete';
+    li.appendChild(deleteButton);
+
+    editButton.addEventListener('click', editTask(index));
+    deleteButton.addEventListener('click', deleteTask(index));
+}
+
+function editTask(){
+
+}
+
+function deleteTask(){
+    tasks.slice(index, 1)
+}
