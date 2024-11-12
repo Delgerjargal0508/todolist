ffe18696c91371a91a643c9c17b352eedd41b0aa
Delgerjargal0508 <deegii0584@gmail.com.com>
2024-11-12 17:10:01 +0800

    working

--- app.js
+++ app.js
@@ -14,25 +14,47 @@ function addTask(){
 
     var li = document.createElement('li');
     li.textContent = newTask;
-    li.setAttribute('data-index', index);
+    var currentIndex = tasks.length - 1;
+    li.setAttribute('data-index', currentIndex);
     taskList.appendChild(li);
 
     var editButton = document.createElement('button');
     editButton.textContent = 'Edit';
     li.appendChild(editButton);
     
-    var deleteButton = document.createElement('button')
+    var deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     li.appendChild(deleteButton);
 
-    editButton.addEventListener('click', editTask(index));
-    deleteButton.addEventListener('click', deleteTask(index));
+    editButton.addEventListener('click', ()=> editTask(li));
+    deleteButton.addEventListener('click', ()=> deleteTask(li));
 }
 
-function editTask(){
+function editTask(li) {
+    const index = li.getAttribute('data-index');
+    const input = document.createElement('input');
+    input.value = tasks[index];
+    const originalText = li.firstChild;
+    li.replaceChild(input, originalText);
+    input.focus();
 
+    input.addEventListener('keyup', function(e) {
+        if (e.key === 'Enter') saveEdit();
+    });
+    input.addEventListener('blur', saveEdit);
+
+    function saveEdit() {
+        tasks[index] = input.value;
+        li.replaceChild(document.createTextNode(input.value), input);
+    }
 }
 
-function deleteTask(){
-    tasks.slice(index, 1)
+function deleteTask(li) {
+    const index = parseInt(li.getAttribute('data-index'));
+    tasks.splice(index, 1);
+    li.remove();
+    const remainingLis = taskList.querySelectorAll('li');
+    remainingLis.forEach((element, newIndex) => {
+        element.setAttribute('data-index', newIndex);
+    });
 }
